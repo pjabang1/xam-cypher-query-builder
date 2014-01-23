c3dfa63b7674574ded793d12bdbb42b58609cce9
42156Pa <42156Pa@YEPS91481.YellGroup.com>
2014-01-23 13:45:29 +0000

    Added Query Builders

---
+++ src/Xam/Cypher/QueryBuilder/CreateQueryBuilder.php
@@ -0,0 +1,15 @@
+<?php
+namespace Xam\Cypher\QueryBuilder;
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+class CreateQueryBuilder implements QueryBuilderInterface {
+    
+    public function build($parameters) {
+        
+    }
+
+}
+?>
---
+++ src/Xam/Cypher/QueryBuilder/QueryBuilderInterface.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Xam\Cypher\QueryBuilder;
+
+/*
+ * QueryBuilderInterface
+ * 
+ */
+
+interface QueryBuilderInterface {
+
+    /**
+     * 
+     * @param array $parameters
+     */
+    public function build($parameters);
+}
+
+?>
