4d2ece898465e0b12d4736c191f1f4ea9fc4c201
42156Pa <42156Pa@YEPS91481.YellGroup.com>
2014-01-23 15:09:33 +0000

    updated phpunit bootstrap file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,7 +1,6 @@
 <?php
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-?>
+error_reporting(E_ALL | E_STRICT);
+
+require_once 'PHPUnit/TextUI/TestRunner.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
