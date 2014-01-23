66335f9a9098dd94b4c59e0576a29dbf7a0d9317
42156Pa <42156Pa@YEPS91481.YellGroup.com>
2014-01-23 15:23:31 +0000

    fixed json_encode error

--- src/Xam/Cypher/QueryBuilder/CreateQueryBuilder.php
+++ src/Xam/Cypher/QueryBuilder/CreateQueryBuilder.php
@@ -19,7 +19,7 @@ class CreateQueryBuilder extends AbstractQueryBuilder {
         $data = $this->getData($parameters['data']);
         
         $label = $this->getLabel($table, $data);
-        $line = json_encode($data);
+        $line = json_encode($data, JSON_FORCE_OBJECT );
         return 'CREATE (' . $label . ' ' . $line . ');';
     }
     
